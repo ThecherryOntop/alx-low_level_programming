5cc85189f7f053ee704d9dff5a4d2640ae88c9aa
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-01 14:00:15 -0700

    print inbetween

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
- * print_to_98 - prints every number from n to 98  * @n: Starting number
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
  */
-void print_to_98(int n)                           {                                                         int i;
-
-        if (n <= 98)
-        {
-                for (i = n; i < 98; i++)
-                {
-                        printf("%i, ", i);
-                }
-        } else
-        {
-                for (i = n; i > 98; i--)
-                {
-                        printf("%i, ", i);
-                }
-        }
-        printf("98\n");
+void print_to_98(int n)
+{
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
+}
+}
+else if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
+}
+else
+{
+printf("98");
+}
+printf("\n");
 }
