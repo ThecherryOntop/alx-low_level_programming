768b71607334ea28a166f3edc3eb151d171a6aad
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-02 10:32:32 -0700

    Collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description - function that multiplies two integers
+ * *@a: the function accepts an input saved into a
+ * *@b: the function accepts an input saved into b
+ * Return: muliply (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
