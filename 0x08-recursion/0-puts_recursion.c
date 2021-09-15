b176dfd29b242e1185742a903d81a961f0e0496a
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-15 08:53:48 -0700

    prints strings

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
-/**
- * A function that prints a string followed by a new line
- *
- * Return: Nothing.
- */
 #include "main.h"
+/**
+* _puts_recursion - Entry point
+* Description - A function that prints a string, followed by a new line
+* *@s: the function accepts an input saved into s
+* Return: Nothing for now
+*/
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
