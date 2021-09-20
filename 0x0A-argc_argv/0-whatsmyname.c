5f999c4497aff39a42598380b94e929c91451133
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-20 13:56:14 -0700

    My ffirst commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * A program that prints its name followed by a new line 
- * 
- * Return: Always 0.
- */
-
-int main(int argc, char* argv[])
+* main - Entry point
+* Description - A program that prints its name followed by a new line  
+* *@argc: the function accepts an input
+* *@argv: the function accepts an input
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
 {
-    printf("My name is \"%s\".\n",argv[0]);
-    return 0;
+printf("0-whatsmyname\"%s\".\n", argv[0]);
+return (0);
 }
