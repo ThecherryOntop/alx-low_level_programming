2672790969552f458dfb8c9f51d565fbbebcb4ce
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-23 19:17:35 -0700

    Pritf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program
+ * Description - "With proper grammar, but the outcome is a piec of art".
+ *
+ * Return: Always zero,
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
