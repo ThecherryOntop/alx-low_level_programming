15b325c7418745ad29f6c2c813c7bb5413b195ef
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-23 19:04:02 -0700

    executables

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - A c program
+ * Description - "Programming is like building a multilingual puzzle".
+ *
+ * Return: Always zero.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
