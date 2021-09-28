19eefad5b5581ff416b6fea0756c78deacb36216
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-09-28 12:56:58 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,76 +1,43 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * new_dog - create new data structure for dog
- * @name: name of dog
- * @age: age of dog
- * @owner: dog owner
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  *
- * Return: Pointer to new dog
- **/
+ * Return: struct dog.
+ * if fails, returns NULL.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int len_name, len_owner;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-
-	len_name = _strlen(name);
-	new_dog->name = malloc(sizeof(char) * len_name + 1);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->name = _strcpy(new_dog->name, name);
-	len_owner = _strlen(owner);
-	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->owner = _strcpy(new_dog->owner, owner);
-	new_dog->age = age;
-
-	return (new_dog);
-}
-
-/**
- * _strlen - determinates the lenght of a string
- * @s: pointer to string
- * Return: the length
- */
-int _strlen(char *s)
+dog_t *p_dog;
+int i, lname, lowner;
+p_dog = malloc(sizeof(*p_dog));
+if (p_dog == NULL || !(name) || !(owner))
 {
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-	;
-	return (a);
+free(p_dog);
+return (NULL);
 }
-
-/**
- * _strcpy - copies a pointed string
- * @dest: pointer to the destine string
- * @src: pointer to the source string
- * Return: the adress of the destiny string
- */
-char *_strcpy(char *dest, char *src)
+for (lname = 0; name[lname]; lname++)
+;
+for (lowner = 0; owner[lowner]; lowner++)
+;
+p_dog->name = malloc(lname + 1);
+p_dog->owner = malloc(lowner + 1);
+if (!(p_dog->name) || !(p_dog->owner))
 {
-	int a = 0;
-
-	while (src[a] != '\0')
-	{
-		dest[a] = src[a];
-		a++;
-	}
-
-	dest[a] = '\0';
-	return (dest);
+free(p_dog->owner);
+free(p_dog->name);
+free(p_dog);
+return (NULL);
+}
+for (i = 0; i < lname; i++)
+p_dog->name[i] = name[i];
+p_dog->name[i] = '\0';
+p_dog->age = age;
+for (i = 0; i < lowner; i++)
+p_dog->owner[i] = owner[i];
+p_dog->owner[i] = '\0';
+return (p_dog);
 }
