6eaa3e1e5667a844cf5efdb596e694ee35d69665
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-10-08 13:57:53 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_list - prints all the elements of a list
+ * @h: a linked list structure
+ * Return: a size_t variable which is a system specific integer
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *temp = h;
+
+	for (count = 0; temp; count++)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
+		temp = temp->next;
+	}
+	return (count);
+}
+
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+**Low level programming: Singly linked lists**
