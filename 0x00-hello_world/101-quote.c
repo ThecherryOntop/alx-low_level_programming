e7c7ba6c427ed577e574505e15f606c8f7d3c40d
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-10-08 19:29:36 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
- * A c program that prints exactly
- * and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19 followed by a new line to the standard error
- * 
- * Return: 1
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\"\n - Dora Korpar, 2015-10-19");
-    return 1;
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
