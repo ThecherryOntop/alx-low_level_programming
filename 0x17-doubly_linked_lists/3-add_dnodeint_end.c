5eb8f18dc05d25bf02c8b8c8a6f432233ea4c4a0
ThecherryOntop <mofeosiyai.mo@gmail.com>
2021-11-25 09:41:02 -0800

    add node at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,57 +1,40 @@
 /*
- * File: 103-keygen.c
+ * File: 3-add_dnodeint_end.c
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "lists.h"
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
  *
- * Return: Always 0.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+	dlistint_t *new, *last;
 
-	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
+	new->n = n;
+	new->next = NULL;
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
-
-	tmp = 1;
-	for (i = 0; i < len; i++)
-		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
-
-	tmp = 0;
-	for (i = 0; i < len; i++)
+	if (*head == NULL)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		new->prev = NULL;
+		*head = new;
+		return (new);
 	}
-	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
 
-	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
-
-	password[6] = '\0';
-	printf("%s", password);
-	return (0);
+	return (new);
 }
